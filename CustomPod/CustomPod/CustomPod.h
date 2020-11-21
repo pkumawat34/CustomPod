038c0c64a30ffb8e5f6f870dec2756614306fa25
Prahalad Kumawat <pkumawat34@gmail.com>
2020-11-21 19:27:15 +0530

    First commit

---
+++ CustomPod/CustomPod/CustomPod.h
@@ -0,0 +1,18 @@
+//
+//  CustomPod.h
+//  CustomPod
+//
+//  Created by Prahalad Kumawat on 21/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomPod.
+FOUNDATION_EXPORT double CustomPodVersionNumber;
+
+//! Project version string for CustomPod.
+FOUNDATION_EXPORT const unsigned char CustomPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomPod/PublicHeader.h>
+
+
---
+++ CustomPod/CustomPod/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  CustomPod
+//
+//  Created by Prahalad Kumawat on 21/11/20.
+//
+
+import Foundation
+
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String{
+        return "Did some stuff, here you go homeboil"
+    }
+}
